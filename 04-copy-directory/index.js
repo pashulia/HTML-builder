5677d995b56b001360ffd1b14cb14c5176e0f3e7
Pavel Khegay <pashulia.pk@gmail.com>
2022-05-16 14:29:38 +0600

    feat: task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+let fs = require('fs/promises');
+let path = require('path');
+let dir = path.join(__dirname, 'files');
+let fileCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(fileCopy, {
+  recursive: true,
+  force: true
+}).finally(function () {
+  fs.mkdir(fileCopy, {
+    recursive: true,
+  });
+  fs.readdir(dir, {
+    withFileTypes: true,
+  }).then(function (data) {
+    data.forEach(function (item) {
+      if (item.isFile()) {
+        let pathItem = path.join(dir, item.name);
+        let pathItemDes = path.join(fileCopy, item.name);
+        fs.copyFile(pathItem, pathItemDes);
+      }
+    });
+  });
+});
